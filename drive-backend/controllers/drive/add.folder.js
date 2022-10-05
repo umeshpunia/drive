4157b4b906e00c98a67669438547f95476a54cf5
umesh <punia.umesh@gmail.com>
2022-10-05 16:05:59 +0530

    create folder location bug fixed

--- drive-backend/controllers/drive/add.folder.js
+++ drive-backend/controllers/drive/add.folder.js
@@ -53,14 +53,14 @@ function createSubFolder(req, res) {
   const { email, name, parentFolderId } = req.body;
 
   if (!email || !name || !parentFolderId)
-    return res.status(400).json({ status: 400, msg: "Bad Request" });
+    return res.json({ status: 400, msg: "Bad Request" });
 
   FolderSchema.findOne({ _id: parentFolderId }, (err, data) => {
     if (err) sendResponse(res, 500, err.message);
     if (!data) sendResponse(res, 400, "No Data Found");
-    var mainFolder = drivePath + "/" + email + "/" + name;
+    var mainFolder = data.location + "/" + name;
     fs.mkdir(mainFolder, (err) => {
-      if (err) return res.status(500).json({ status: 500, msg: err.message });
+      if (err) return res.json({ status: 500, msg: err.message });
 
       let insFolder = new FolderSchema({
         createdBy: email,
@@ -71,9 +71,8 @@ function createSubFolder(req, res) {
 
       insFolder.save((err, data) => {
         console.log(data);
-        if (err) return res.status(501).json({ status: 501, msg: err.message });
-        if (!data)
-          return res.status(401).json({ status: 401, msg: "Please Try Again" });
+        if (err) return res.json({ status: 501, msg: err.message });
+        if (!data) return res.json({ status: 401, msg: "Please Try Again" });
 
         res
           .status(200)
